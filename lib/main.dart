6ed662109b9ecb265bb19c8d57b768f81d4559c9
FlutLab.io <root@vmi348945.contaboserver.net>
2020-09-17 20:13:00 +0200

    test snap 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
+//gfdgfd
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
